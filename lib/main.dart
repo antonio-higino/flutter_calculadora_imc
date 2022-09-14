7d843d85392bd5ea04283bebeab30d13f836feef
Antonio Higino B L Medeiros <56856275+antonio-higino@users.noreply.github.com>
2022-09-14 13:23:32 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Calculadora de IMC", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {}
